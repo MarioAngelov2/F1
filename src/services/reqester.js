bedc86556916b3be533b6ba213a3639aff2afae3
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-04 22:29:41 +0300

    Add request for driver standings

--- src/services/reqester.js
+++ src/services/reqester.js
@@ -1,4 +1,15 @@
-export function getAllRaces() {
+export function getCurrentSeason() {
   return fetch(`http://ergast.com/api/f1/current.json`)
     .then(res => res.json())
 }
+
+export function getDriversStandings() {
+  return fetch(`http://ergast.com/api/f1/2023/driverStandings.json`)
+    .then(res => res.json())
+}
+
+export function getAllSeasons() {
+  return fetch(`http://ergast.com/api/f1/seasons.json`)
+    .then(res => res.json())
+}
+
