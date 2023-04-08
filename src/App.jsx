be4cda5d1c17968b048140298090a04b9e6a5d07
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-08 15:30:34 +0300

    Add route to home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Races from "./components/Races";
 import DriverStandings from "./components/DriverStandings";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="/home" element={<Home />} />
         <Route path="/races" element={<Races />} />
         <Route path="/driver-standings" element={<DriverStandings />} />
       </Routes>
