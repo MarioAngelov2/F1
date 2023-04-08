1d6e0efac91d13d15984dad10a9aed159791a980
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-08 15:30:52 +0300

    Create slider data util

---
+++ src/utils/sliderData.js
@@ -0,0 +1,17 @@
+import imageOne from '../assets/F1-image1.jpg'
+import imageTwo from '../assets/F1-image2.jpg'
+
+export const sliderData = [
+  {
+    title: 'F1 Passion',
+    path: '/home',
+    image: imageOne,
+    alt: 'img-car'
+  },
+  {
+    title: 'F1 Passion',
+    path: '/home',
+    image: imageTwo,
+    alt: 'img-car'
+  }
+]
