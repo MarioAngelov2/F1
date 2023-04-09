7cd61d244f2ba520a65db2be9129d015b5b6574b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-09 10:54:18 +0300

    Update sliderData.js

--- src/utils/sliderData.js
+++ src/utils/sliderData.js
@@ -1,17 +1,38 @@
-import imageOne from '../assets/F1-image1.jpg'
-import imageTwo from '../assets/F1-image2.jpg'
+import imageOne from "../assets/F1-image1.jpg";
+import imageTwo from "../assets/F1-image2.jpg";
+import imageThree from "../assets/F1-image3.jpg";
+import imageFour from "../assets/F1-image4.jpg";
+import imageFive from "../assets/F1-image5.jpg";
 
 export const sliderData = [
   {
-    title: 'F1 Passion',
-    path: '/home',
+    title: "F1 Passion",
+    path: "/home",
     image: imageOne,
-    alt: 'img-car'
+    alt: "img-car",
   },
   {
-    title: 'F1 Passion',
-    path: '/home',
+    title: "F1 Passion",
+    path: "/home",
     image: imageTwo,
-    alt: 'img-car'
-  }
-]
+    alt: "img-car",
+  },
+  {
+    title: "F1 Passion",
+    path: "/home",
+    image: imageThree,
+    alt: "img-car",
+  },
+  {
+    title: "F1 Passion",
+    path: "/home",
+    image: imageFour,
+    alt: "img-car",
+  },
+  {
+    title: "F1 Passion",
+    path: "/home",
+    image: imageFive,
+    alt: "img-car",
+  },
+];
