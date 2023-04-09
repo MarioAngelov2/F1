8ab5b6ec59bdaff6e54a7e52b999f18f2cd11207
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-09 20:40:08 +0300

    Update DriverStandings.jsx

--- src/components/DriverStandings.jsx
+++ src/components/DriverStandings.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
+
 import { getDriversStandings } from "../services/reqester";
-import BeatLoader from "react-spinners/BeatLoader";
 import DriverStandingsData from "./DriverStandingsData";
+import { motion } from "framer-motion";
 
 import "../style/DriverStandings.css";
 
@@ -22,34 +23,41 @@ const DriverStandings = () => {
   }, []);
 
   return (
-    <div className="driverStandings-container">
-      <main>
-        <div className="main-content">
-          <div className="season-progress">
-            {driverInfo.map((standingList) => (
-              <React.Fragment key={standingList.season}>
-                <h3>Season {standingList.season}</h3>
-                <h4>Round {standingList.round}</h4>
-              </React.Fragment>
-            ))}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="driverStandings-container">
+        <main>
+          <div className="main-content">
+            <div className="season-progress">
+              {driverInfo.map((standingList) => (
+                <React.Fragment key={standingList.season}>
+                  <h3>Season {standingList.season}</h3>
+                  <h4>Round {standingList.round}</h4>
+                </React.Fragment>
+              ))}
+            </div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Position</th>
+                  <th>Driver</th>
+                  <th>Nationality</th>
+                  <th>Car</th>
+                  <th>Pts</th>
+                </tr>
+              </thead>
+              <tbody>
+                <DriverStandingsData />
+              </tbody>
+            </table>
           </div>
-          <table>
-            <thead>
-              <tr>
-                <th>Position</th>
-                <th>Driver</th>
-                <th>Nationality</th>
-                <th>Car</th>
-                <th>Pts</th>
-              </tr>
-            </thead>
-            <tbody>
-             <DriverStandingsData />
-            </tbody>
-          </table>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </motion.div>
   );
 };
 
