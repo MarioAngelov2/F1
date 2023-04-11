a7ed4d59217d540870130ce4544f4799392c08c3
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-11 23:31:27 +0300

    Add season progress info

--- src/components/ConstructorsStandigs.jsx
+++ src/components/ConstructorsStandigs.jsx
@@ -1,13 +1,33 @@
 import { useState, useEffect } from "react";
-import ConstructorsStandigsData from '../components/ConstructorsStandigsData'
+import ConstructorsStandigsData from "../components/ConstructorsStandigsData";
+import { getConstructorStandings } from "../services/reqester";
 
-import '../style/ConstructorsStandings.css'
+import "../style/ConstructorsStandings.css";
 
 const ConstructorsStandigs = () => {
+  const [constructorData, setConstructorData] = useState([]);
+  console.log(constructorData.map(x => console.log(x.season)))
+
+  useEffect(() => {
+    getConstructorStandings().then((result) => {
+      setConstructorData(
+        Object.values(result.MRData.StandingsTable.StandingsLists)
+      );
+    });
+  }, []);
+
   return (
     <div className="constructorStandings-container">
       <main>
         <div className="main-content">
+          <div className="constructor-progress">
+            {constructorData.map((constructor) => (
+              <>
+                <h2>Season {constructor.season}</h2>
+                <h3>Round {constructor.round}</h3>
+              </>
+            ))}
+          </div>
           <table>
             <thead>
               <tr>
