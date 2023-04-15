b3892a32d52e98f562e27a7c9dbd333fc1a0f1ab
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-15 19:14:20 +0300

    Update TeamList.jsx

--- src/components/Teams/TeamList.jsx
+++ src/components/Teams/TeamList.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { teamList } from "../../utils/teamList";
+import TeamListData from "./TeamListData";
 
 import "../../style/Teams.css";
 
@@ -7,37 +7,7 @@ const TeamList = () => {
   return (
     <div className="drivers-container">
       <div className="row">
-        {teamList.map((team) => (
-          <div key={team.teamName} className="listing-container">
-            <div className="listing-wrapper">
-              <div className="listing-item">
-                <div className="listing-standing">
-                  <div className="rank">{team.teamPosition}</div>
-                  <div className="points">
-                    <div className="team-pts">{team.teamPoints}</div>
-                    <div className="pts-logo">Pts</div>
-                  </div>
-                </div>
-                <div className="listing-info">
-                  <div className="team-info">
-                    <span>{team.teamName}</span>
-                    <div className="team-logo">
-                      <img src={team.teamLogo} />
-                    </div>
-                  </div>
-
-                  <div className="listing-drivers">
-                    <div className="driver">{team.firstDriver}</div>
-                    <div className="driver">{team.secondDriver}</div>
-                  </div>
-                  <div className="team-car-image">
-                    <img src={team.carImage} />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
+        <TeamListData />
       </div>
     </div>
   );
