e6dbaac1ff88e3c8ab942eeb69d62d8fb6f7bd93
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-15 13:34:52 +0300

    Minor refactor

--- src/components/Teams/TeamList.jsx
+++ src/components/Teams/TeamList.jsx
@@ -15,16 +15,17 @@ const TeamList = () => {
                   <div className="rank">{team.teamPosition}</div>
                   <div className="points">
                     <div className="team-pts">{team.teamPoints}</div>
-                    <div className="pts-logo">Pts</div>                  
+                    <div className="pts-logo">Pts</div>
                   </div>
                 </div>
                 <div className="listing-info">
                   <div className="team-info">
                     <span>{team.teamName}</span>
+                    <div className="team-logo">
+                      <img src={team.teamLogo} />
+                    </div>
                   </div>
-                  <div className="team-logo">
-                    <img src={team.teamLogo} />
-                  </div>
+
                   <div className="listing-drivers">
                     <div className="driver">{team.firstDriver}</div>
                     <div className="driver">{team.secondDriver}</div>
@@ -44,3 +45,41 @@ const TeamList = () => {
 
 export default TeamList;
 
+// return (
+//   <div className="drivers-container">
+//     <div className="row">
+//       {teamList.map((team) => (
+//         <div key={team.teamName} className="listing-container">
+//           <div className="card">
+//             <div className="card-body">
+//               <div className="card-standingInfo">
+//                 <div className="ranking">
+//                   {team.teamPosition}
+//                   </div>
+//                 <div className="points">
+//                   <div className="team-pts">{team.teamPoints}</div>
+//                   <div className="logo-pts">Pts</div>
+//                 </div>
+//               </div>
+//               <div className="card-TeamInfo">
+//                 <div className="teamName">
+//                   <span>{team.teamName}</span>
+//                   <div className="teamLogo">
+//                     <img src={team.teamLogo} />
+//                   </div>
+//                 </div>
+//                 <div className="card-drivers">
+//                   <div className="driver">{team.firstDriver}</div>
+//                   <div className="driver">{team.secondDriver}</div>
+//                 </div>
+//                 <div className="team-car-image">
+//                   <img src={team.carImage} />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   </div>
+// );
