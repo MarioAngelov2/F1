ec49938ef84ff6eb28ddb9d6227df2487c6ed919
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-16 11:31:27 +0300

    Update reqester.js

--- src/services/reqester.js
+++ src/services/reqester.js
@@ -1,20 +1,17 @@
 export function getCurrentSeason() {
-  return fetch(`http://ergast.com/api/f1/current.json`)
-    .then(res => res.json())
+  return fetch(`http://ergast.com/api/f1/current.json`).then((res) =>
+    res.json()
+  );
 }
 
 export function getDriversStandings() {
-  return fetch(`http://ergast.com/api/f1/2023/driverStandings.json`)
-    .then(res => res.json())
+  return fetch(`http://ergast.com/api/f1/2023/driverStandings.json`).then(
+    (res) => res.json()
+  );
 }
 
 export function getConstructorStandings() {
-  return fetch(`http://ergast.com/api/f1/2023/constructorStandings.json`)
-    .then(res => res.json())
+  return fetch(`http://ergast.com/api/f1/2023/constructorStandings.json`).then(
+    (res) => res.json()
+  );
 }
-
-// export function getAllSeasons() {
-//   return fetch(`http://ergast.com/api/f1/seasons.json`)
-//     .then(res => res.json())
-// }
-
