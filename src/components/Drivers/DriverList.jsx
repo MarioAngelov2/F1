9463c59f00298a4c40d23b0e1bf6936bca47796f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-17 16:31:45 +0300

    Add page animation

--- src/components/Drivers/DriverList.jsx
+++ src/components/Drivers/DriverList.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import DriverListData from "./DriverListData";
+import { motion } from "framer-motion";
 
-import '../../style/Drivers.css'
+import "../../style/Drivers.css";
 
 const DriverList = () => {
   return (
-    <div className="drivers-container">
-      <div className="title">
-        <h1>F1 DRIVERS 2023</h1>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="drivers-container">
+        <div className="title">
+          <h1>F1 DRIVERS 2023</h1>
+        </div>
+        <div className="micro-text">
+          <p>
+            Check out this season's official F1 line-up. Full breakdown of
+            drivers, points and current positions. Follow your favourite F1
+            drivers on and off the track.
+          </p>
+        </div>
+        <div className="drivers-row">
+          <DriverListData />
+        </div>
       </div>
-      <div className="micro-text">
-        <p>
-          Check out this season's official F1 line-up. Full breakdown of
-          drivers, points and current positions. Follow your favourite F1
-          drivers on and off the track.
-        </p>
-      </div>
-      <div className="drivers-row">
-        <DriverListData />
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
