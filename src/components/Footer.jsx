f1200a47c76a460f6e1ba623d09bd45290c256b7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-22 19:34:43 +0300

    Add clipboard to the email icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/Footer.css";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
-import { Box } from "@mui/material";
+import { Box, Button, Popover } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const [copied, setCopied] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const emailAdddress = "mario.angelov995@gmail.com";
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(emailAdddress);
+    setCopied(true);
+    setTimeout(() => setCopied(false, 3000));
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    console.log(event)
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : "undefined";
+
   return (
     <div className="footer-container">
       <div className="footer-info">
@@ -14,9 +37,30 @@ const Footer = () => {
           className="icons-container"
         >
           <Link to={`https://github.com/MarioAngelov2`}>
-            <GitHubIcon fontSize="large" sx={{ mr: 2, color: "white" }} />
+            <GitHubIcon
+              className="icon1"
+              fontSize="large"
+              sx={{ mr: 2, color: "white" }}
+            />
           </Link>
-          <EmailIcon fontSize="large" />
+          <Button
+            onClick={(event) => {
+              copyToClipboard();
+              handleClick(event);
+            }}
+          >
+            <EmailIcon className="icon2" fontSize="large" />
+          </Button>
+          <Popover
+            
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          >
+            The email is copied!
+          </Popover>
         </Box>
         <span className="author">Author: Mario Angelov</span>
       </div>
