48a082593d6bb15d952c68bbf6262665c05f3bcd
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-22 15:26:04 +0300

    Refactor hero page with new design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,88 +1,47 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import carImg from "../assets/Haas.jpeg";
 import "../style/Home.css";
-import { sliderData } from "../utils/SliderData";
 import { motion } from "framer-motion";
-
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import NavigateBeforeOutlinedIcon from "@mui/icons-material/NavigateBeforeOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import { Button } from "@mui/material";
 
 const Home = () => {
-  const [current, setCurrent] = useState(0);
-  const length = sliderData.length;
-  const timeOut = useRef(null);
-
-  // change the background with setTimeout
+  const navigate = useNavigate();
   useEffect(() => {
-    const nextSlide = () => {
-      setCurrent((current) => (current === length - 1 ? 0 : current + 1));
-    };
-
-    timeOut.current = setTimeout(nextSlide, 5000);
-
-    return function () {
-      if (timeOut.current) {
-        clearTimeout(timeOut.current);
-      }
-    };
-  }, [current, length]);
-
-  const nextSlide = () => {
-    if (timeOut.current) {
-      clearTimeout(timeOut.current);
-    }
-
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    if (timeOut.current) {
-      clearTimeout(timeOut.current);
-    }
-
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
-  // check if the array is empty
-  if (!Array.isArray(sliderData) || sliderData.length <= 0) {
-    return null;
-  }
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
       exit={{ opacity: 0 }}
+      className="home-container"
     >
-      <div className="home-container">
-        <div className="content-wrapper">
-          {sliderData.map((slide, index) => (
-            <div className="content-slide" key={index}>
-              {index === current && (
-                <>
-                  <motion.div
-                    initial={{ opacity: 0.7 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 3 }}
-                    className="content-slider"
-                  >
-                    <div className="content-info">
-                      <h2>{slide.title}</h2>
-                    </div>
-                    <motion.img className="img-slide" src={slide.image} />
-                  </motion.div>
-                </>
-              )}
-            </div>
-          ))}
-          <div className="arrow-buttons">
-            <NavigateBeforeOutlinedIcon
-              onClick={prevSlide}
-              className="arrow-button"
-            />
-            <NavigateNextIcon onClick={nextSlide} className="arrow-button" />
-          </div>
+      <motion.div
+        animate={{ y: 0 }}
+        initial={{ y: "100%" }}
+        transition={{ duration: 0.8 }}
+        className="header-title"
+      >
+        <h1>The Power Behind the Podium: Meet the Leading F1 Teams</h1>
+        <div className="img-container">
+        <img src={carImg} />
         </div>
-      </div>
+      </motion.div>
+      <motion.div
+        animate={{ x: 0 }}
+        initial={{ x: "100%" }}
+        transition={{ duration: 0.4, delay: 0.5 }}
+        className="btn"
+      >
+        <span>Check out the current driver lineup</span>
+        <Button onClick={() => navigate("/drivers")}>
+          <ArrowForwardIosOutlinedIcon className="arrow-btn"></ArrowForwardIosOutlinedIcon>
+        </Button>
+      </motion.div>
     </motion.div>
   );
 };
