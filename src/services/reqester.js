b3b379d5418b9a40feadd31cbbd78c35826e6209
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-24 19:12:00 +0300

    Add HashRouter to main.jsx

--- src/services/reqester.js
+++ src/services/reqester.js
@@ -1,7 +1,5 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
 export function getCurrentSeason() {
-  return fetch(`${API_BASE_URL}current.json`).then((res) => {
+  return fetch(`https://ergast.com/api/f1/current.json`).then((res) => {
     if (res.status === 200) {
       return res.json();
     } else {
@@ -11,23 +9,29 @@ export function getCurrentSeason() {
 }
 
 export function getDriversStandings() {
-  return fetch(`${API_BASE_URL}2023/driverStandings.json`).then((res) => {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      throw new Error(`getDriversStandings failed with status: ${res.status}`);
+  return fetch(`https://ergast.com/api/f1/2023/driverStandings.json`).then(
+    (res) => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        throw new Error(
+          `getDriversStandings failed with status: ${res.status}`
+        );
+      }
     }
-  });
+  );
 }
 
 export function getConstructorStandings() {
-  return fetch(`${API_BASE_URL}2023/constructorStandings.json`).then((res) => {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      throw new Error(
-        `getConstructorStandings failed with status: ${res.status}`
-      );
+  return fetch(`https://ergast.com/api/f1/2023/constructorStandings.json`).then(
+    (res) => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        throw new Error(
+          `getConstructorStandings failed with status: ${res.status}`
+        );
+      }
     }
-  });
+  );
 }
