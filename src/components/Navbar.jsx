0d95353dbc567f08b69f652bed51b7b795b24654
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-24 19:01:22 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="links" id={showLinks ? "hidden" : ""}>
-        <Link to="https://f1-gamma.vercel.app/races">Race Schedule</Link>
-        <Link to="https://f1-gamma.vercel.app/driver-standings">Driver Standings</Link>
-        <Link to="https://f1-gamma.vercel.app/constructor-standings">Constructor Standings</Link>
-        <Link to="https://f1-gamma.vercel.app/teams">Teams</Link>
-        <Link to="https://f1-gamma.vercel.app/drivers">Drivers</Link>
+        <Link to="/races">Race Schedule</Link>
+        <Link to="/driver-standings">Driver Standings</Link>
+        <Link to="/constructor-standings">Constructor Standings</Link>
+        <Link to="/teams">Teams</Link>
+        <Link to="/drivers">Drivers</Link>
       </div>
       <Button
         className="hamburger-btn"
